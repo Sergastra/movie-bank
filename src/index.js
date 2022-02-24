3f5d5dd864ad990a0ae69f01e60cf313bcadf37e
Sergey <sergastrah@yandex.ru>
2022-02-25 01:02:46 +0400

    Build base for app

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function Form(props){
   };
 
   
-  return(
+  return (
     <div id= " form-container">
       <label for= 'title'> Title </label>
       <input 
@@ -39,35 +39,83 @@ function Form(props){
         onClick={ handleClick }
       />
     </div>
-  )
+  );
 }
 
+function Results(props) {
+  const { title, results} = props;
 
+  if(results) {
+    return (
+    <>
+      <h2>{ title }</h2>
+      <div id= "result-container">
+        { results.map(( result, i ) => {
+          return <Movie key={i} details = { result } />;
+        })}
 
-function App(){
+      </div>
+    </>
+  );
+  }else {
+   return <h2>Not found</h2>
+  }
+}
+
+function Movie(props) {
+  const details = props.details;
+  console.log(details);
+
+  return (
+    <div className="movie-details">
+
+      <figure>
+        <img
+          src={ details.Poster }
+          alt={`The poster movie`}
+        />
+      </figure>
+      <h3> { details.Title } </h3>
+      <h3> { details.Year } </h3>
+      <h3> { details.Type } </h3>
+
+      <input
+        type="button"
+        value="детали"
+      />
+    </div>
+  );
+}
+
+function App() {
+  const [ titles, setTitles ] = useState([]);
+  const [ results, setResults ] = useState([]);
 
   const handleClick = title => {
-    console.log(title);
-    
+    setTitles(title);
+
     let api_key = 'edb978d';
-    let url = 'http://www.omdbapi.com/?apikey= ${api_key}&s= ${title}';
-
-
+    let url = `http://www.omdbapi.com/?apikey=${api_key}&s=${title}`;
+    
     axios.get(url)
-      .then(response => console.log('response :>>', response));
+        .then(response => {console.log('response :>>', response)
+          setResults( response.data.Search )
+    });
   };
 
   return (
-    <>
-    <h1> Movie Bank</h1>
-    <Form 
-      onClick={ handleClick } 
-    />
-    </>
-  )
-};
-
+      <>
+        <h1> Movie Bank</h1>
+        <Form onClick={ handleClick } />
+        <Results 
+          title={ titles }
+          results={ results } 
+        />
+      </>
+  );
+  
+  
+}
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
